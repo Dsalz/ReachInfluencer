18f2d7cf56ed0f9ebecc4f5a9f1e72ab372324a6
Damola <salisu.damola@yahoo.com>
2019-04-22 21:31:11 +0100

    add analytics page

--- assets/js/site.js
+++ assets/js/site.js
@@ -29,14 +29,120 @@ notificationsDiv.addEventListener('click', (e) => {
   e.stopPropagation();
 })
 
-modalBtn.addEventListener('click', () => {
-  modalWrapper.classList.toggle("show-modal");
-})
+if(modalBtn && modalDiv && modalWrapper){
+  modalBtn.addEventListener('click', () => {
+    modalWrapper.classList.toggle("show-modal");
+  })
+  
+  modalWrapper.addEventListener('click', () => {
+    modalWrapper.classList.toggle("show-modal");
+  })
+  
+  modalDiv.addEventListener('click', (e) => {
+    e.stopPropagation();
+  })
+}
 
-modalWrapper.addEventListener('click', () => {
-  modalWrapper.classList.toggle("show-modal");
-})
 
-modalDiv.addEventListener('click', (e) => {
-  e.stopPropagation();
-})
+
+Highcharts.chart('social-dist', {
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: false,
+    type: 'pie'
+  },
+  title: {
+    text: ''
+  },
+  tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  }, 
+  legend: {
+    enabled: false
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: 'pointer',
+      dataLabels: {
+        enabled: false
+      },
+      showInLegend: false
+    }
+  },
+  series: [{
+    name: 'Distribution',
+    colorByPoint: true,
+    data: [{
+      name: 'Youtube',
+      y: 61.41,
+      color: '#ED3833'
+    }, {
+      name: 'Instagram',
+      y: 11.84,
+      color: '#E223E2'
+    }, {
+      name: 'Twitter',
+      y: 10.85,
+      color: '#38A1F3'
+    }]
+  }]
+});
+
+Highcharts.chart('analytics-category-dist', {
+  chart: {
+    type: 'column',
+    height: '300',
+    width: '900'
+  },
+  title: {
+    text: ''
+  },
+  subtitle: {
+    text: ''
+  },
+  xAxis: {
+    categories: [
+      'Food',
+      'Health',
+      'Sports',
+      'Music',
+      'Movies',
+      'Tech',
+      'Books',
+      'Art',
+    ],
+    title: {
+      text: 'Categories'
+    },
+    crosshair: true
+  },
+  yAxis: {
+    min: 0,
+    title: {
+      text: 'Influencers'
+    }
+  },
+  tooltip: {
+    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+      '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+    footerFormat: '</table>',
+    shared: true,
+    useHTML: true
+  },
+  plotOptions: {
+    column: {
+      pointPadding: 0.2,
+      borderWidth: 0
+    }
+  },
+  series: [{
+    name: 'Influencers',
+    data: [20, 11, 30, 5, 15, 16, 27, 34],
+    color: 'rgba(6, 180, 254, .1)'
+  },]
+});
+
+
